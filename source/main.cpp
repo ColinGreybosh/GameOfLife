42877608d9d7909f1e3d90d7a6647a808de0e283
ColinGreybosh <cgreybosh99@gmail.com>
2017-09-14 20:06:40 -0400

    Change constant variable naming

--- source/main.cpp
+++ source/main.cpp
@@ -5,14 +5,15 @@
 int main(int argc, char* args[])
 {
     // Configuration Settings //
-    const int worldWidth   = 360;
-    const int worldHeight  = 180;
-    const double windowScale = 5.0; // Window size as a percentage of the world size. Ex: 50x50 world with 2.0 scale = 100x100 window
-    const double isAliveChance = 0.5; // Percentage chance that a specific cell will be alive at world generation if RANDOM is the arg in CellVector.generateseed()  
-    const char* gameTitle = "Conway's Game of Life";
+    const int WORLD_WIDTH   = 720;
+    const int WORLD_HEIGHT  = 360;
+    const int PREFERRED_FPS = 60;
+    const double WINDOW_SCALE = 2.0; // Window size as a percentage of the world size. Ex: 50x50 world with 2.0 scale = 100x100 window
+    const double IS_ALIVE_CHANCE = 0.5; // Percentage chance that a specific cell will be alive at world generation if RANDOM is the arg in CellVector.generateseed()  
+    const char* GAME_TITLE = "Conway's Game of Life";
 
-    Game game(worldWidth, worldHeight, windowScale, gameTitle);
-    game.generateSeed(isAliveChance);
+    Game game(WORLD_WIDTH, WORLD_HEIGHT, WINDOW_SCALE, GAME_TITLE);
+    game.generateSeed(IS_ALIVE_CHANCE);
 
     bool running = true;
     while (running)
